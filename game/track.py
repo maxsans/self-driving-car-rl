c025d07735b1861061df30e85d393da5f1321526
maxsans <maxence.menard@reseau.eseo.fr>
2024-12-10 11:27:46 +0100

    add checkpoints

--- game/track.py
+++ game/track.py
@@ -14,7 +14,19 @@ class Track:
         # Créer un masque pour les bordures du circuit (pixels blancs)
         self.mask = pygame.mask.from_threshold(self.image, (255, 255, 255), (10, 10, 10))
 
+        # Define checkpoints as rectangles (lines of height 60 pixels)
+        self.checkpoints = [
+            pygame.Rect(510, 169, 5, 60),
+            pygame.Rect(656, 402, 5, 60),
+            pygame.Rect(132, 501, 5, 60),
+            pygame.Rect(250, 70, 5, 60)
+        ]
+
     def draw(self, surface):
         # Dessiner l'image de la piste à la position (0, 0)
         surface.blit(self.image, (0, 0))
+
+        # Draw checkpoints
+        # for checkpoint in self.checkpoints:
+        #     pygame.draw.rect(surface, (0, 255, 0), checkpoint)
 
