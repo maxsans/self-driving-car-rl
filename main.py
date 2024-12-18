29adaa99e2bc5585e7d4e1f90358818eeeca2dea
Jules <jules.dempt@outlook.fr>
2024-12-18 09:18:05 +0100

    feat: manual control on game env

--- main.py
+++ main.py
@@ -1,46 +1,51 @@
 # main.py
 
 import pygame
-from game.car import Car
-from game.engine import GameEngine
-from game.track import Track
-from settings import WINDOW_WIDTH, WINDOW_HEIGHT, FPS
-from envs.game_env import CarRacingEnv
-import gymnasium
-from stable_baselines3.common.env_checker import check_env
+
+from envs.game_env import CarRacingEnv, Throttle, Steering
+
+
+def main():
+    env = CarRacingEnv(render_mode="human")
+    obs = env.reset()
+    terminated = False
+
+    while not terminated:
+        # action = env.action_space.sample()  # Replace with your action selection logic
+
+        throttle = Throttle.NO_ACTION.value
+        steering = Steering.NO_ACTION.value
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            throttle = Throttle.ACCELERATE.value
+        elif keys[pygame.K_DOWN]:
+            throttle = Throttle.BRAKE.value
+        if keys[pygame.K_LEFT]:
+            steering = Steering.TURN_LEFT.value
+        elif keys[pygame.K_RIGHT]:
+            steering = Steering.TURN_RIGHT.value
+
+        obs, reward, terminated, truncated, info = env.step((throttle, steering))
+
+    print("Done")
+
+    env.close()
+
+if __name__ == "__main__":
+    main()
 
 
 
 #
 # def main():
-#     env = CarRacingEnv(render_mode="human")
-#     obs = env.reset()
-#     terminated = False
+#     pygame.init()
+#     screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 #
-#     while not terminated:
-#         action = env.action_space.sample()  # Replace with your action selection logic
-#         obs, reward, terminated, truncated, info = env.step(action)
-#         # env.render()
+#     engine = GameEngine(screen)
+#     engine.run()
 #
-#     print("Done")
+#     pygame.quit()
 #
-#     env.close()
-#
-# if __name__ == "__main__":
-#     main()
-
-
-
-
-def main():
-    pygame.init()
-    screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-
-    engine = GameEngine(screen)
-    engine.run()
-
-    pygame.quit()
-
 
 if __name__ == "__main__":
     main()
